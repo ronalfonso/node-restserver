f4ec82f752f06649207112d7b582cfde23dc2d7d
ronalfonso <raalfonsoparra@gmail.com>
2018-09-14 11:08:14 -0400

    Coneccion a la base de datos local

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
+
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
@@ -43,6 +47,14 @@ app.put('/usuario/:id', function (req, res) {
 
 app.delete('/usuario', function (req, res) {
     res.json('delete Usuario')
+});
+
+mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+
+    if ( err ) throw err;
+
+    console.log('Base de datos Online');
+
 });
 
 app.listen(process.env.PORT, () => {
